37e28e86cd02b0c292935d5cac7427228f379793
Henrique Reinaldi <carlosofalmeida@gmail.com>
2025-06-01 13:16:50 -0300

    Add files via upload

--- otracker!/otracker.py
+++ otracker!/otracker.py
@@ -1,26 +1,46 @@
-#aiohttp
 
 import asyncio
 from datetime import date
 from ossapi import OssapiAsync
 day = date.today()
+name = ""
 class jogatina:
     def __init__(self):
         self.titulo = None
+
+        self.stars = 0
+        self.pp = 0
         self.acuracia = 0
         self.ar = 0
         self.cs = 0
         self.od = 0
-        self.hp = 0
         self.rank = None
 
+        self.mods = []
+        self.combo = 0
+        self.miss = 0
+        self.total = 0
+        self.score = 0
+
+        self.ss = 0
+
+
     def list(self):
-        print("="*20)
-        print(self.titulo, ": ")
-        print("Estatisticas:")
+        print("="*20,"\n")
+        print(self.titulo, ": [", self.stars, "* | ", round(self.pp, 1), "pp]",sep="") if self.pp != None else print(self.titulo, ": [", self.stars, "*]",sep="")
+
+        print("\nEstatisticas:")
         print("Acuracia: ", self.acuracia, " | Rank: ", self.rank)
-        print("cs: ", self.cs, " ar: ", self.ar, " od: ", self.od, " hp:", self.hp)
-        print("="*20)
+        print("cs: ", self.cs, " ar: ", self.ar, " od: ", self.od)
+        print("Combo: ", self.combo, " - ", self.miss, "x / ", self.total, " - Score: ", self.score ,sep="")
+
+        print("\nMods:")
+        for mod in self.mods:
+            print(mod.acronym, end=" ")
+
+        print("\nScoreStar: ", self.ss)
+
+        print("\n")
         return
 
 try:
@@ -38,6 +58,8 @@ try:
             current = current+char
     info.append(current)
 
+    if info[2]:
+        name = info[2]
     api = OssapiAsync(int(info[0]), str(info[1])) #idcliente e senha. pra cirar vai no site do osu configuracoes da conta (leia docs)
 except:
     print("Coloca as porra no app.txt direito desgrama:")
@@ -49,27 +71,40 @@ async def getStuff(player):
     global api
     usere = await api.user(player)
 
-    return(await api.user_scores(usere, type="recent", include_fails=True, limit=1000)) #recente -> 24horas
+    return(await api.user_scores(usere, type="recent", include_fails=True, limit=1000)) #recent -> 24horas
 
 def process(list):
     plays = []
     for item in list:
-        if item.started_at != None and item.started_at.date() != day:
+        if item.ended_at != None and item.ended_at.date() == day:
             play = jogatina()
             play.titulo = item.beatmapset.title
             play.acuracia = round(item.accuracy*100 ,3)
             play.ar = item.beatmap.ar
             play.cs = item.beatmap.cs
             play.od = item.beatmap.accuracy
-            play.hp = item.beatmap.drain
             play.rank = item.rank
+
+            play.mods = item.mods
+            play.pp = item.pp
+            play.stars = item.beatmap.difficulty_rating
+
+            play.score = item.total_score
+            play.combo = item.max_combo
+            play.miss = item.statistics.miss if item.statistics.miss != None else 0
+            play.total = item.beatmap.count_circles + item.beatmap.count_sliders + item.beatmap.count_spinners
+
+            play.ss = round((play.score / 1000000) * play.stars, 2)
 
             plays.append(play)
 
-    print("Escores de ", day, ":")
-
     for play in plays:
         play.list()
+        pass
 
 while True:
-    process(asyncio.run(getStuff(input("Player --> "))))
+    if name != "":
+        process(asyncio.run(getStuff(name)))
+        break
+    else:
+        process(asyncio.run(getStuff(input("Player --> "))))
